6fa6dc669714556a2ca6ccee35e3e4dbde70b26a
faizAhmed10 <faizahmed10604@gmail.com>
2024-09-08 16:32:12 +0530

    commit

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -2,7 +2,7 @@ from pathlib import Path
 from datetime import timedelta
 import os
 from decouple import config
-
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -11,12 +11,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)n3!48ovpvmd**#zq@^#0pfynrw^jt@)dogo8tm3xese+ljt_r'
-# SECRET_KEY = config('SECRET_KEY')
+# SECRET_KEY = 'django-insecure-)n3!48ovpvmd**#zq@^#0pfynrw^jt@)dogo8tm3xese+ljt_r'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
+# DEBUG = True
+DEBUG = config('DEBUG')
 ALLOWED_HOSTS = ['*']
 # ALLOWED_HOSTS = config('ALLOWED_HOSTS')
 
@@ -84,14 +84,18 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # }
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST'),  # Render gives you this info
-        'PORT': '5432',  # Default PostgreSQL port
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': config('DB_NAME'),
+    #     'USER': config('DB_USER'),
+    #     'PASSWORD': config('DB_PASSWORD'),
+    #     'HOST': config('DB_HOST'),  # Render gives you this info
+    #     'PORT': '5432',  # Default PostgreSQL port
+    # }
+    
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')  # or os.getenv('DATABASE_URL')
+    )
 }
 
 REST_FRAMEWORK = {
